c54539deae0cf802e6f1f3a1069123d5dcfedeae
Jordan Chadsey <jordan@identipop.com>
2018-01-17 22:50:26 -0800

    file structure

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes for API and views
+// app.use(routes);
+
+// Mongo
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
+  {
+    useMongoClient: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🤓 ✌️ ==> API Server now listening on PORT ${PORT}!`)
+);
