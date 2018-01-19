25be5575027ed5706112c0f501b6df423c27ec25
Jordan Chadsey <jordan@identipop.com>
2018-01-18 19:45:38 -0800

    filestack image render testing

--- client/src/components/Filestack/Filestack.js
+++ client/src/components/Filestack/Filestack.js
@@ -2,16 +2,29 @@ import React from "react";
 import "./Filestack.css";
 import ReactFilestack, {client} from 'filestack-react';
 
-const Filestack = props =>
+class Filestack extends React.Component {
+
+state = {
+  image: ""
+}
+
+makeImage =(result) => {
+  this.setState({image: result.filesUploaded[0].url})
+}
+  render(){
+    return(
 <div>
   <ReactFilestack
     apikey="AdP2fF1FvRdGW4RWCKw8sz"
     buttonText="Click me"
     buttonClass="classname"
     options=""
-    onSuccess={this.yourCallbackFunction}
+    onSuccess={this.makeImage}
   />
-
+  <img src = {this.state.image}/>
 </div>
 
+);
+}
+}
 export default Filestack;
