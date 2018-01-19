be9c93816e67d1d57cc3768b231851ea38d79ac5
Yash Rajgor <yash592@gmail.com>
2018-01-18 22:50:40 -0800

    Routes

---
+++ controllers/cardController.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+
+// Methods for cardController
+
+module.exports = {
+
+	findAll: function(req, res) {
+
+	},
+
+	findById: function(req, res) {
+
+	},
+
+	create: function(req, res) {
+
+	},
+
+	remove: function(req, res) {
+
+	}
+};
+
