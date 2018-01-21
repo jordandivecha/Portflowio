0367a7ed154d733d909c7ca28998b4c018117e58
Yash Rajgor <yash592@gmail.com>
2018-01-20 19:51:16 -0800

    Initial components

--- client/src/components/Cards/CardElement.js
+++ client/src/components/Cards/CardElement.js
@@ -2,3 +2,24 @@
 // primarily styling
 // can use a b-strap or materialize preset card
 // src for image/other content will come from props
+
+import React from "react";
+import "./CardElement.css";
+
+const PortflowioCard = props => (
+
+	<div className="card">
+	  <div className="img-container">
+	    <img class="materialboxed" alt = {} width="650" src={} />
+	  </div>
+	  <div className="content">
+	    
+	  </div>
+	  <span onClick={() => } className="remove">
+	    𝘅
+	  </span>
+	</div>
+
+	);
+
+export default PortflowioCard;
