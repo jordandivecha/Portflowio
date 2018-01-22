a335b99799335a8033fd0b2f149b44ddb99b1c68
Kevin <kcorso89@gmail.com>
2018-01-21 22:31:06 -0800

    yashtag functionality (no styling yet)

---
+++ client/src/components/yashtags/yashtags.js
@@ -0,0 +1,20 @@
+// YAAAASHTAAAGS
+
+import React from "react";
+import "./yashtags.css";
+
+const Yash = () => (
+	<div className="yashtag-container">
+		<div className="chips"></div>
+  		<div className="chips chips-initial"></div>
+  		<div className="chips chips-placeholder"></div>
+  		<div className="chips chips-autocomplete"></div>
+		<div className="chip">
+    		Yashtag
+    		<i className="close material-icons">close</i>
+  		</div>
+
+	</div>
+)
+
+export default Yash;
