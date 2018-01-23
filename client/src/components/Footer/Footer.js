0b2dc4e561a283802e287089331d8209e934e098
Jordan Chadsey <jordan@identipop.com>
2018-01-22 21:25:03 -0800

    post model

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,13 +4,13 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => (
-	<div class="footer-dark">
+	<div className="footer-dark">
     <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+        <div className="container">
+            <div className="row">
+                <div className="col item social"><a href="#"><i className="icon ion-social-facebook"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-instagram"></i></a></div>
             </div>
-            <p class="copyright">Portflowio © 2018</p>
+            <p className="copyright">Portflowio © 2018</p>
         </div>
     </footer>
 </div>
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,36 @@
 const path = require("path");
-const router = require("express").Router();
+
 
 const apiRoutes = require("./api");
+// models = require('./models'),
+// passport = require('passport'),
+// _ = require('underscore'),
+async = require('async'),
+stream_node = require('getstream-node'),
+// fs = require('fs'),
+// bodyParser = require('body-parser'),
+// methodOverride = require('method-override');
 
-
+router = require("express").Router();
+// User = models.User,
+// Post = models.Post,
+// Like = models.Like,
+// Follow = models.Follow;
+//
+// var FeedManager = stream_node.FeedManager;
+// var StreamMongoose = stream_node.mongoose;
+// var StreamBackend = new StreamMongoose.Backend();
+// var enrichActivities = function(body) {
+// 	var activities = body.results;
+// 	return StreamBackend.enrichActivities(activities);
+// };
+// var enrichAggregatedActivities = function(body) {
+// 	var activities = body.results;
+// 	return StreamBackend.enrichAggregatedActivities(activities);
+// };
 router.use("/api", apiRoutes);
 
-// router.use("/",(req, res) =>
-// 	res.sendFile(path.join(__dirname, "../client/build/index.html"))
-// );
+
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
