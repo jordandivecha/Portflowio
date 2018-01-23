016513dddb70036cdbbc1ee76b79bae252357ff1
Kevin <kcorso89@gmail.com>
2018-01-22 22:30:31 -0800

    3.0 take 1

--- client/src/components/yashtags/yashtags.js
+++ client/src/components/yashtags/yashtags.js
@@ -4,15 +4,34 @@ import React from "react";
 import "./yashtags.css";
 import {Chip, Tag, Col, Row} from 'react-materialize'
 
-const Yash = () => (
+class Yash extends React.component {
 
+	state = {
+		yashtag: ""
+	};
 
+	handleInputChange = event => {
+	const { name, value } = event.target;
+    	this.setState({
+     	 	yashtag: value
+    	});
+  	};
 
-			 <Tag></Tag>
-
-
-
-
-)
+	render(){
+		return(
+	<form>
+		<div className="form-group">
+      		<input
+        		className="yashtag-input"
+        		type="text"
+        		value={this.state.yashtag}
+        		name="q"
+        		onChange={this.handleInputChange}
+        	/>
+        	<Tag>{this.state.yashtag}</Tag>
+    	</div>
+	</form>
+	)};
+}
 
 export default Yash;
