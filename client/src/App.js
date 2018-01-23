37532626e28dffa3a61680c9695194d37c19d923
Jordan Chadsey <jordan@identipop.com>
2018-01-23 10:29:39 -0800

    modal in app

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
           <ul className= "right">
             <Link to="/"><button type="button" className= "btn btn-info">Home</button></Link>
             <Link to ="/profile"><button type="button" className= "btn btn-danger">Profile</button></Link>
-
+            <ModalElement/>
 
           </ul>
         </div>
