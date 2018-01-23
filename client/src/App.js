541eec87bef2bd7a1bad0c04ba88cab51b515c54
Yash Rajgor <yash592@gmail.com>
2018-01-23 11:59:15 -0800

    Fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -38,11 +38,8 @@ class App extends Component {
       <Router>
         <div>
           <Header/>
-        <div>
-          <ul className= "right">
-            <Link to="/"><button type="button" className= "btn btn-info">Home</button></Link>
-            <Link to ="/profile"><button type="button" className= "btn btn-danger">Profile</button></Link>
-            <ModalPost/>
+       
+          
 
         <Security  issuer= {config.issuer}
                    client_id= {config.client_id}
@@ -53,6 +50,7 @@ class App extends Component {
                       <ul className= "right">
                         <Link to="/"><button type="button" className= "btn btn-info">Home</button></Link>
                         <Link to ="/profile"><button type="button" className= "btn btn-danger">Profile</button></Link>
+                        <ModalPost/>
                         
 
                       </ul>
