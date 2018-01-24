0d5c17784e5c63cf2a33e8a30d490dcbee13ae1f
Yash Rajgor <yash592@gmail.com>
2018-01-24 11:48:42 -0800

    Parallax 3.0

--- client/src/components/Parallax/Parallax.js
+++ client/src/components/Parallax/Parallax.js
@@ -4,10 +4,17 @@ import React from "react";
 
 import {Parallax} from "react-materialize";
 
+// import Header from "../Header/Header.js"
+import Portflowio from "../Cards/CardElement.js"
+
 const ScrollImage = () => (
   <div class = "scroll">
-      <Parallax imageSrc="https://i.imgur.com/Hi4YqyS.jpg"/>
-      <Parallax imageSrc="https://i.imgur.com/Hi4YqyS.jpg"/>
+
+      <Parallax imageSrc="https://i.imgur.com/Hi4YqyS.jpg" />    
+
+      <Portflowio />
+      
+      
   </div>
 );
 
